5d3648c20b3dc59dcb270b5a8a6f920e8af4d49a
Shital Shah <shitals@microsoft.com>
2019-01-08 08:04:02 -0800

    remove x_f param from log, add throttle, skip_mod itertool

--- regim/longview_probe.py
+++ regim/longview_probe.py
@@ -7,11 +7,12 @@ class LongviewProbe(Probe):
         super(LongviewProbe, self).__init__(exp_name, run_name, epoch_config, model, 
             callbacks, metrics, log_settings)
         self.lv = lv.WatchServer()
-        self.lv.log_globals(model=self.model, metrics=metrics.stats)
+        self.metrics = metrics
+        self.lv.log_globals(model=model, metrics=metrics.stats)
 
     def on_after_batch(self, train_test, input, label, output, loss):
         super(LongviewProbe, self).on_after_batch(train_test, input, label, output, loss)
-        self.lv.log_event("batch", x_f=lambda e, *_:e.metrics.epochf,
+        self.lv.log_event("batch", x=self.metrics.epochf,
             input=input, label=label, output=output, loss=loss)
 
     def on_after_epoch(self, test_train, loader):
