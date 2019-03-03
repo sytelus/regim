73891c98f13239be1ca712e7e44d1b526626e466
Shital Shah <shitals@microsoft.com>
2019-03-03 13:51:23 -0800

    new shortcut APIs, TextPrinter, lots of refactoring

--- regim/longview_probe.py
+++ regim/longview_probe.py
@@ -8,19 +8,19 @@ class LongviewProbe(Probe):
             callbacks, metrics, log_settings)
         self.lv = lv.WatchServer()
         self.metrics = metrics
-        self.lv.set_vars(model=model, metrics=metrics.stats)
+        self.lv.set_globals(model=model, metrics=metrics.stats)
 
         #TODO manage client server connections better
         #lv.wait_key("Press any key to continue...")
 
     def on_after_batch(self, train_test, batch_state):
         super(LongviewProbe, self).on_after_batch(train_test, batch_state)
-        self.lv.set_vars("batch", x=self.metrics.stats.epochf, batch=batch_state, tt=train_test)
+        self.lv.observe("batch", x=self.metrics.stats.epochf, batch=batch_state, tt=train_test)
 
     def on_after_epoch(self, train_test, loader):
         super(LongviewProbe, self).on_after_epoch(train_test, loader)
         self.lv.end_event("batch")
-        self.lv.set_vars("epoch", tt=train_test)
+        self.lv.observe("epoch", tt=train_test)
 
     #TODO log end epoch
 
