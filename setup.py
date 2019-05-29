62544695de5577821848d42f0d0f2f585fd0e366
Shital Shah <shital@ShitalShah.com>
2019-05-29 00:07:47 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="regim",
-    version="0.8.2",
+    version="0.8.3",
     author="Shital Shah",
     author_email="sytelus@gmail.com",
     description="PyTorch Train Test Regiment Pipeline",
